fb151f30ec8b26851218185d70cd7b2380726df3
Rick Vellinga <rickvellinga2408@gmail.com>
2023-04-24 11:09:51 +0200

    URL toevoegen

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 
 const baseURL = 'https://api.vervoerregio-amsterdam.fdnd.nl/api/v1'
 const partnerSlug = '/websites'
+const postSlug = '/urls'
 
 const partner_data = await fetch(baseURL + partnerSlug). then((response) => response.json())
 
@@ -13,6 +14,9 @@ const app = express()
 // Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Gebruik de map 'public' voor statische resources
 app.use(express.static('public'))
@@ -31,7 +35,23 @@ app.get('/toolboard', function (req, res) {
 })
 
 app.get('/urltoevoegen', function (req, res) {
-  res.render('urltoevoegen', {active: '/urltoevoegen'})
+  res.render('urltoevoegen', {partner_data, active: '/urltoevoegen'})
+})
+
+app.post('/urltoevoegen', function(req, res) {
+  const formURL = baseURL + postSlug
+  postJson(formURL, req.body).then((data) => {
+    let newURL = { ... req.body }
+    console.log(JSON.stringify(data))
+    if (data.data) {
+      res.redirect('/projectboard') 
+    } else {
+      const errormessage = `${req.body.url}: URL bestaat al.`
+      const newdata = { error: errormessage, values: newURL }
+      
+      res.render('urltoevoegen', {newdata, partner_data, active: '/urltoevoegen' })
+    }
+  })
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -42,3 +62,28 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+
+/**
+ * postJson() is a wrapper for the experimental node fetch api. It fetches the url
+ * passed as a parameter using the POST method and the value from the body paramater
+ * as a payload. It returns the response body parsed through json.
+ * @param {*} url the api endpoint to address
+ * @param {*} body the payload to send along
+ * @returns the json response from the api endpoint
+ */
+
+async function postJson(url, body) {
+  return await fetch(url, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .catch((error) => error)
+}
